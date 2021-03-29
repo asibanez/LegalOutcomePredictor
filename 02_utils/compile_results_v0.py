148c60716758dd4090509640cd8d094375ea3002
Santiago_Ibanez <sibanez@sloan.mit.edu>
2021-03-29 09:22:28 -0400

    updated

---
+++ 02_utils/compile_results_v0.py
@@ -0,0 +1,83 @@
+#%% Imports
+import json
+import pickle
+import pandas as pd
+from sklearn.metrics import confusion_matrix
+import matplotlib.pyplot as plt
+
+#%% Path definitions
+path_results = 'C:/Users/siban/Dropbox/CSAIL/Projects/12_Legal_Outcome_Predictor/00_data/02_runs/batch_02/art_06/11_art6_50p_att_v4_2_50ep_att_TEST/full_results.json'
+path_test_set_preprocessed = 'C:/Users/siban/Dropbox/CSAIL/Projects/12_Legal_Outcome_Predictor/00_data/01_preprocessed/case_EN_test_df.pkl'
+path_test_set_model = 'C:/Users/siban/Dropbox/CSAIL/Projects/12_Legal_Outcome_Predictor/00_data/01_preprocessed/01_article_split/art_06_50p_par_att/model_test.pkl'
+
+#%% Global initialization
+tp, tn, fp, fn = 0, 0, 0, 0
+
+#%% Load data
+with open(path_results, 'r') as fr:
+    results = json.load(fr)
+
+with open(path_test_set_model, 'rb') as fr:
+    test_model_df = pickle.load(fr)
+
+with open(path_test_set_preprocessed, 'rb') as fr:
+    test_preproc_df = pickle.load(fr)
+
+#%%
+predictions = [int(x) for x in results['Y_test_prediction_binary']]
+ground_truth = [int(x) for x in results['Y_test_ground_truth']]
+case_id = test_model_df['case_id']
+
+#%% Build main dataframe
+data_df = pd.DataFrame({'case_id': case_id,
+                        'predictions':predictions,
+                        'ground_truth': ground_truth})
+
+#%% Compute number of case paragraphs per entry
+num_case_passages = []
+for case_id in data_df['case_id']:
+    entry = test_preproc_df[test_preproc_df.ITEMID == case_id]
+    num_passages = len(entry['TEXT'].item())
+    num_case_passages.append(num_passages)
+
+data_df['num_case_passages'] = num_case_passages
+
+#%% Compute number of violated article paragraphs
+num_violated_pars = []
+for case_id in data_df['case_id']:
+    entry = test_model_df[test_model_df.case_id == case_id]
+    num_violated_pars_aux = sum(entry['violated_pars'].item())
+    num_violated_pars.append(num_violated_pars_aux)
+
+data_df['num_violated_pars'] = num_violated_pars
+
+#%% Slice wrong and correct predictions
+data_df_wrong = data_df[data_df.predictions != data_df.ground_truth]
+data_df_correct = data_df[data_df.predictions == data_df.ground_truth]
+
+#%% Compute metrics
+for _, entry in data_df.iterrows():
+    prediction = entry['predictions']
+    ground_truth = entry['ground_truth']
+    if prediction == 1 and ground_truth == 1: tp += 1
+    if prediction == 0 and ground_truth == 0: tn += 1
+    if prediction == 1 and ground_truth == 0: fp += 1
+    if prediction == 0 and ground_truth == 1: fn += 1
+
+#%%    
+precision = tp / (tp + fp)
+recall = tp / (tp + fn)
+f1 = 2 * precision * recall / (precision + recall)
+
+#%% Print metrics
+print(f'Precision = {precision}')
+print(f'Recall = {recall}')
+print(f'F1 = {f1}')
+
+#%% Plot stats
+_ = plt.hist(data_df_correct.num_case_passages)
+_ = plt.hist(data_df_wrong.num_case_passages)
+_ = plt.hist(data_df_correct.num_violated_pars)
+_ = plt.hist(data_df_wrong.num_violated_pars)
+
+
--- 02_utils/dataset_reader_v1.py
+++ 02_utils/dataset_reader_v1.py
@@ -3,11 +3,10 @@
 #%% Imports
 
 import pickle
-from pprint import pprint
 
 #%% Path definition
 
-path_data = 'C:/Users/siban/Dropbox/CSAIL/Projects/12_Legal_Outcome_Predictor/00_data/01_preprocessed/01_article_split/art_06_50p_par/model_train.pkl'
+path_data = 'C:/Users/siban/Dropbox/CSAIL/Projects/12_Legal_Outcome_Predictor/00_data/01_preprocessed/01_article_split/art_06_50p_par_att/model_train.pkl'
 path_tok_2_id = 'C:/Users/siban/Dropbox/CSAIL/Projects/12_Legal_Outcome_Predictor/00_data/01_preprocessed/tok_2_id_dict.pkl'
 
 #%% Data loading
@@ -20,47 +19,50 @@ with open(path_tok_2_id, 'rb') as fr:
 
 #%% Global initialization
 
-pos = 0
-n_case_texts = 300
+pos = 10259
+n_case_texts = 50
 n_art_pars = 11
-len_case_texts = 512
+seq_len = 512
 id_2_tok = {}
-art_par_strings = []
-case_strings_all = {}
+art_pars_strings = []
+case_strings = []
 
 #%% tok_2_id generation
 
 for key in tok_2_id.keys():
     id_2_tok[tok_2_id[key]] = key
     
-#%% Article extraction
+#%% Extract from dataframe
 
 case_id = data.iloc[pos]['case_id']
-art_id = data.iloc[pos]['article_id']
-art_par_ids = data.iloc[pos]['article_pars_ids']
-art_par_tok = [id_2_tok[x] for x in art_par_ids]
+ECHR_art_id = data.iloc[pos]['article_id']
+art_pars_ids = data.iloc[pos]['article_pars_ids']
+case_texts_ids = data.iloc[pos]['case_texts_ids']
+violated_pars_ids = data.iloc[pos]['violated_pars']
 
-#%%
+#%% Convert ids to tokens
+art_pars_tok = [id_2_tok[x] for x in art_pars_ids]
+case_texts_tok = [id_2_tok[x] for x in case_texts_ids]
+
+#%% Article extraction
 for idx in range(0, n_art_pars):
-    aux = art_par_tok[idx*len_case_texts : (idx + 1)*len_case_texts]
+    aux = art_pars_tok[idx * seq_len : (idx + 1) * seq_len]
     aux = [x for x in aux if x != '<PAD>']
     aux = (' ').join(aux)
-    art_par_strings.append(aux)
-for x in art_par_strings: print(x,'\n')
-
+    if aux != '':
+        art_pars_strings.append(aux)
+    
 #%% Case extraction
-
-case_ids_all = data.iloc[pos]['case_texts_ids']
-case_tok_all = [id_2_tok[x] for x in case_ids_all]
-
-#%%
 for idx in range(0, n_case_texts):
-    case_tok = case_tok_all[idx*len_case_texts : (idx+1)*len_case_texts]
-    case_strings_all[idx] = (' ').join([x for x in case_tok if x != '<PAD>'])
-    #case_strings_all[idx] = (' ').join([x for x in case_tok])
+    aux = case_texts_tok[idx * seq_len : (idx+1) * seq_len]
+    aux = [x for x in aux if x != '<PAD>']
+    aux = (' ').join(aux)
+    if aux != '':
+        case_strings.append(aux)
+    
+#%% Print example
 
-#%% Print results
-
-print(f'CASE ID:\n{case_id}\n')
-print(f'ARTICLE PARAGRAPHS:\n{art_par_strings}\n')
-_ = [print(f'CASE_TEXT {x}\n{case_strings_all[x]}\n') for x in range(0,n_case_texts)]
+print(f'CASE ID: {case_id}\n')
+print(f'ECHR art id: {ECHR_art_id}')
+_ = [print(f'ART_PARAGRAPH {idx}\n{text}\n') for idx, text in enumerate(art_pars_strings)]
+_ = [print(f'CASE_TEXT {idx}\n{text}\n') for idx, text in enumerate(case_strings)]
